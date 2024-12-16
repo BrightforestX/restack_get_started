36aeac8022a2ef30bccb215d3a1662adbdcd7518
Clifford Dalson III <cdalsoniii@gmail.com>
2024-12-15 20:04:44 -0500

    latest changes

--- restack_up.py
+++ restack_up.py
@@ -1,11 +1,19 @@
 import os
 from restack_sdk_cloud import RestackCloud
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
+
+print(f"GIT_URL: {os.getenv('GIT_URL')}")
+print(f"GIT_BRANCH: {os.getenv('GIT_BRANCH')}")
 
 async def main():
     # Initialize the RestackCloud client with the SDK token from environment variables
     restack_cloud_client = RestackCloud(os.getenv('RESTACK_CLOUD_TOKEN'))
+    git_url = os.getenv('GIT_URL', 'default_git_url')
+    git_branch = os.getenv('GIT_BRANCH', 'default_git_branch')
+    print("git_url: ", git_url)
+    print("git_branch: ", git_branch)
+
 
     engine = {
         'name': 'restack_engine', # IMPORTANT: This must match the name of the engine in the Restack Cloud console.
@@ -35,7 +43,11 @@ async def main():
               'name': 'RESTACK_ENGINE_API_KEY',
               'value': os.getenv('RESTACK_ENGINE_API_KEY'),
           },
+          {'name': 'gitUrl', 'value': os.getenv('GIT_URL')},
+          {'name': 'gitBranch', 'value': os.getenv('GIT_BRANCH')}
         ],
+        'gitUrl': os.getenv('GIT_URL'),
+        'gitBranch': os.getenv('GIT_BRANCH'),
     }
 
     # Define the application configuration
@@ -43,6 +55,8 @@ async def main():
         'name': 'defense_quickstart',
         'dockerFilePath': '/examples/defense_quickstart/Dockerfile',
         'dockerBuildContext': './examples/defense_quickstart/',
+        'gitUrl': git_url,
+        'gitBranch': git_branch,
         'environmentVariables': [
             {
                 'name': 'RESTACK_ENGINE_ID',
@@ -60,7 +74,11 @@ async def main():
                 'name': 'OPENBABYLON_API_URL',
                 'value': os.getenv('OPENBABYLON_API_URL', ''),
             },
+            {'name': 'gitUrl', 'value': os.getenv('GIT_URL')},
+            {'name': 'gitBranch', 'value': os.getenv('GIT_BRANCH')}
         ],
+    'gitUrl': os.getenv('GIT_URL'),
+    'gitBranch': os.getenv('GIT_BRANCH'),
     }
 
     # Configure the stack with the applications
