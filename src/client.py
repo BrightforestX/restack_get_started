757e1ca161f5fa9052bd9b32af4f86f2fb7e4b44
martinibach <martin@restack.io>
2024-12-16 16:58:57 +0100

    Refactor client.py to use CloudConnectionOptions for Restack client initialization

--- src/client.py
+++ src/client.py
@@ -1,4 +1,11 @@
 import os
 from restack_ai import Restack
+from dotenv import load_dotenv
 
-client = Restack()
+connection_options = CloudConnectionOptions(
+    engine_id=os.getenv('RESTACK_ENGINE_ID'),
+    address=os.getenv('RESTACK_ENGINE_ADDRESS'),
+    api_key=os.getenv('RESTACK_ENGINE_API_KEY')
+)
+
+client = Restack(connection_options)
